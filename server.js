24d82504979e906d86d5a448fa5458fe2745c57c
Varunrajotiya <88929725+Varunrajotiya@users.noreply.github.com>
2025-08-05 21:44:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/api/jobs', (req, res) => {
+    console.log('Job posted:', req.body);
+    res.json({ success: true, message: 'Job posted successfully' });
 });
 
 app.listen(PORT, () => {
